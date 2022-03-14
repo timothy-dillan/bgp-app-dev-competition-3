f11451b40212df10eb026fd07b6c875cc517888c
ahmad.rafsanjani <ahmad.rafsanjani@BA-00022990.local>
2022-03-14 19:21:52 +0700

    add react routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
+import LandingPage from './pages/landing'
+import ReactLanding from './pages/defaultreact'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={ <ReactLanding />} />
+      <Route path="/landing" element={<LandingPage /> } />
+    </Routes>
+   </div>
   );
 }
 
