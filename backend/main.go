435ad840db847a51f7dace36b855fd5fd13d6b4d
Timothy Dillan <timothy.tandjung@tokopedia.com>
2022-03-14 15:50:43 +0700

    feat(initial commit): initialize backend with boilerplate code

---
+++ backend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: initialize logging
+
+	// TODO: initialize database
+
+	// TODO: initialize redis
+
+	// TODO: initialize routes
+	fmt.Println("Hello, world!")
+}
---
+++ backend/server/http.go
@@ -0,0 +1,8 @@
+package server
+
+import "fmt"
+
+func main() {
+	// TODO: add routes
+	fmt.Println("Hello, HTTP!")
+}
