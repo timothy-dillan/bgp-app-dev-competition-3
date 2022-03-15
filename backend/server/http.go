9bd15c44e0b6b2d28b94bac6db9581c1dde56cfc
Timothy Dillan <timothy.tandjung@tokopedia.com>
2022-03-16 02:23:50 +0700

    feat(refactor): move cors middleware

--- backend/server/http.go
+++ backend/server/http.go
@@ -9,7 +9,7 @@ import (
 
 func InitHTTP() error {
 	router := gin.Default()
-	router.Use(CORSMiddleware())
+	router.Use(middleware.CORSConfiguration())
 
 	// TODO: add routes
 	router.GET("/ping", func(c *gin.Context) {
@@ -33,21 +33,4 @@ func InitHTTP() error {
 	}
 
 	return nil
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		c.Header("Access-Control-Allow-Origin", "http://localhost:4000")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
--- backend/server/middleware/middleware.go
+++ backend/server/middleware/middleware.go
@@ -26,3 +26,13 @@ func Authenticate() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func CORSConfiguration() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:4000")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH,GET,PUT")
+		c.Next()
+	}
+}
