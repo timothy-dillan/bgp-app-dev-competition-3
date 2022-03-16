2df8039fdf5ecd02474af12c5ba04fd2fc1d96ec
nethaniasonya <nethania.sonya@ui.ac.id>
2022-03-16 20:28:03 +0700

    fix query & import

--- backend/init.sql
+++ backend/init.sql
@@ -6,7 +6,9 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     display_name varchar(255) NOT NULL,
     username varchar(255) NOT NULL,
-    password varchar(255) NOT NULL
+    password varchar(255) NOT NULL,
+    gopay_coins_balance float NOT NULL DEFAULT 10000,
+    gopay_balance float NOT NULL DEFAULT 100000
 );
 
 INSERT INTO users (display_name, username, password) VALUES ('Alice Wonderland', 'alice', '$2a$10$F2K.H5e0OBugzP2Ichtu8utM3Ns4ekrmHwbEHsg4TESd7MOrYyHAC');
